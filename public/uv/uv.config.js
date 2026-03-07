6128bf663144be0d33cc00615e69c7d88c4dd97c
nickMzBR <nickmuniz1212@gmail.com>
2026-03-06 22:31:53 -0300

    Atualizar o uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,1 +1,10 @@
-00000
+self.__uv$config = {
+  prefix:    "/service/",
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler:   "/uv/uv.handler.js",
+  bundle:    "/uv/uv.bundle.js",
+  config:    "/uv/uv.config.js",
+  sw:        "/uv/uv.sw.js",
+  bare:      "/api/bare",
+};
